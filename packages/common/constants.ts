39dd4471c4162dd2f3ca066ec6fef884d3a59ff2
Datzu712 <devdatzucl@gmail.com>
2022-07-31 17:25:27 -0600

    Command hooks decoratos

--- packages/common/constants.ts
+++ packages/common/constants.ts
@@ -2,3 +2,4 @@ export const COMMAND_ARGS_METADATA = '__commandExecutionArguments__';
 export const PARAM_TYPES_METADATA = 'design:paramtypes';
 export const COMMAND_WATERMARK = '__command__';
 export const COMMAND_METADATA = '__commandMetadata__';
+export const COMMAND_HOOKS_METADATA = '__commandHooks__';
