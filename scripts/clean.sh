cf1281b8b68d93be343506ed0be93890a24d9c5a
Datzu712 <devdatzucl@gmail.com>
2022-07-30 21:06:15 -0600

    Now clean script ignore .json files

--- scripts/clean.sh
+++ scripts/clean.sh
@@ -5,6 +5,12 @@ do
     # check if the filename ends with .js or .js.map or .d.ts
     if [[ $file == *".js"* || $file == *".js.map"* || $file == *".d.ts"* ]]
     then
+        if [[ $file == *".json"* ]]
+        then
+            # skip json files
+            continue
+        fi
+        rm $file
         # remove the file
         echo "Removed $file"
     fi
